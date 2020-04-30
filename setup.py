56f7ee689ee0da0e82dfd8c850256caa6cdb0d90
Michał Martyniak <michal.martyniak@deepsense.ai>
2020-04-30 13:14:08 +0200

    Adjust setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from pathlib import Path
-from setuptools import setup
+from setuptools import setup, find_packages
 
-REPO_ROOT = Path(__file__).parent
-README = (REPO_ROOT / "README.md").read_text()
-with open(REPO_ROOT / "requirements.txt") as f:
+README = Path("README.md").read_text()
+with open("requirements.txt") as f:
     REQUIREMENTS = f.read().splitlines()
 
 setup(
-    name="carla_birdeye_view",
-    version="1.0",
+    name="carla-birdeye-view",
+    version="1.0.12",
     description="Bird-eye's view for CARLA simulator",
     keywords=["CARLA", "birdview", "bird-eye's view", "Reinforcement Learning", "RL"],
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/deepsense-ai/carla_birdeye_view",
+    url="https://github.com/deepsense-ai/carla-birdeye-view",
     author="Michał Martyniak",
     author_email="michal.martyniak@linux.pl",
     license="MIT",
@@ -24,9 +23,10 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.8,"
     ],
     include_package_data=True,
     install_requires=REQUIREMENTS,
-    entry_points={"console_scripts": ["birdview-demo=birdview.__main__:main"]},
+    packages=find_packages(),
+    entry_points={"console_scripts": ["birdview-demo = carla_birdeye_view.__main__:main"]},
 )
